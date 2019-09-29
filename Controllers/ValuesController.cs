a9f8f4b2f8edadf7f38380b6d8d1303001223440
Prime <stephan.gie@gmail.com>
2019-09-29 19:19:50 +0200

    Anpassungen für AuthRepo

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DatingApp.api.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace DatingApp.api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ValuesController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public WeatherForecastController(DataContext context)
+        public ValuesController(DataContext context)
         {
             _context = context;
         }
