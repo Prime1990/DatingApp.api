176b71d116cf079f3e631c5637751f1a44d32794
Prime <stephan.gie@gmail.com>
2019-09-30 19:48:37 +0200

    initial Version von Klasse

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,10 +1,17 @@
 using System.Threading.Tasks;
 using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.api.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DbContext _context;
+
+        public AuthRepository(DbContext context)
+        {
+            _context = context;
+        }
         public Task<User> Login(string username, string password)
         {
             throw new System.NotImplementedException();
